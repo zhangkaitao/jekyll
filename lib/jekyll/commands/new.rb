45b640cb44dea333c88da64800d9fc9f560074de
Tom Preston-Werner <tom@mojombo.com>
2013-01-17 13:23:32 -0800

    Fix spelling and link errors in site generator.

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -13,8 +13,8 @@ module Jekyll
           { "layout" => "post", "title" => "Welcome to Jekyll!", "date" => Time.now.strftime('%Y-%m-%d %H:%M:%S'), "categories" => %w(jekyll update) }.to_yaml + "---",
           "You'll find this post in your `_posts` directory - edit this post and re-build (or run with the `-w` switch) to see your changes!",
           "To add new posts, simply add a file in the `_posts` directory that follows the convention: YYYY-MM-DD-name-of-post.ext.",
-          "Check out the [Jeyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll's GitHub repo][jekyll-gh].",
-          "[jekyll-gh]: https://github.com/mojombo/github\n[jekyll]:    http://jekyllrb.com"
+          "Check out the [Jekyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll's GitHub repo][jekyll-gh].",
+          "[jekyll-gh]: https://github.com/mojombo/jekyll\n[jekyll]:    http://jekyllrb.com"
         ]
         f.write(content.join("#{$/*2}"))
       end
